34567ef60bc51875a9c6c9e75666228f8a0f5dce
MohammadParsaghorbani <147430428+MohammadParsaghorbani@users.noreply.github.com>
2023-10-28 14:35:17 +0330

    Update set.py

---
+++ set.py
@@ -0,0 +1,11 @@
+#set
+print("=============================================")
+num_1=int(input("enter first number: "))
+num_2=int(input("enter second number: "))
+num_3=int(input("enter 3ed number" ))
+num_4=int(input("enter 4ed number" ))
+num_5=int(input("enter 5ed number" ))
+num_6=int(input("enter 6ed number" ))
+my_set={num_1,num_2,num_3,num_4,num_5,num_6}
+for set in range(1):
+    print(my_set)
