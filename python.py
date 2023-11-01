4d2bfeceb763a6689855cb5f0b730b937784f391
MohammadParsaghorbani <147430428+MohammadParsaghorbani@users.noreply.github.com>
2023-11-01 17:08:05 +0330

    Add files via upload

---
+++ python.py
@@ -0,0 +1,15 @@
+a=float(input("enter your math score: "))
+b=float(input("enter your physic score: "))
+c=float(input("enter your chemistery score: "))
+d=float(input("enter your sinse score: "))
+e=float(input("enter your arabic score: "))
+f=float(input("enter your Engilish score: "))
+g=float(input("enter your music_art score: "))
+h=float(input("enter your writing score: "))
+i=float(input("enter your camputer score: "))
+j=float(input("enter your art score: "))
+
+def score(score):
+    print(score/10,"this is your avraeg")
+
+score(a+b+c+d+e+f+g+h+i+j)
