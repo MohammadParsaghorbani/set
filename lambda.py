718b83f4ae2b795dd7af4298848842d615aca22f
MohammadParsaghorbani <147430428+MohammadParsaghorbani@users.noreply.github.com>
2023-11-08 19:47:04 +0330

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -15,3 +15,20 @@ elif oprator=="*":
     print(c(num1,num2))
 elif oprator=="/":
     print(d(num1,num2))
+more=str(input("do you want to add new number?y/n "))
+
+while more=="y":
+    num1=int(input("enter first number: "))
+    num2=int(input("enter second number: "))
+    oprator=str(input("enter oprator: "))
+    if oprator=="+":
+        print(a(num1,num2))
+    elif oprator=="-":
+        print(b(num1,num2))
+    elif oprator=="*":
+        print(c(num1,num2))
+    elif oprator=="/":
+        print(d(num1,num2))
+    more=str(input("do you want to add new number?y/n "))
+    if more=="n":
+        break
