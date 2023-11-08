59c6ffc8b2baaeb266cc1e67c838d51f412629ad
MohammadParsaghorbani <147430428+MohammadParsaghorbani@users.noreply.github.com>
2023-11-08 19:49:36 +0330

    Update func.py

--- func.py
+++ func.py
@@ -20,3 +20,19 @@ elif oprator=="*":
     third(num1)
 elif oprator=="/":
     fourth(num1)
+more=str(input("do you want to add new number?y/n "))
+while more=="y":
+    num1=int(input("enter first number: "))
+    num2=int(input("enter second number: "))
+    oprator=str(input("enter oprator: "))
+    if oprator=="+":
+        first(num1)
+    elif oprator=="-":
+        second(num1)
+    elif oprator=="*":
+        third(num1)
+    elif oprator=="/":
+        fourth(num1)
+    more=str(input("do you want to add new number?y/n "))
+    if more=="n":
+        break
